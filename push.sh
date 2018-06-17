8543b387085c5e0e25044cb5a5c04e57c0d16dcb
Richie Bendall <richiebendall@gmail.com>
2018-06-17 17:54:21 +1200

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  git checkout -b gh-pages
+  git add files/.gitignore
+  git add files/.gitattributes
+  git commit --message "Updated Git Files"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/Richienb/git-files.git > /dev/null 2>&1
+  git push origin master
+}
+
+setup_git
+commit_files
+upload_files
